399de995134f6ab563d986b4e3502b762fd30c27
przemekdab1993 <przemekdab1993@gmail.com>
2022-09-18 15:17:12 +0200

    4dodanie komponentów adnotation i twig

---
+++ app/src/Controller/HomeController.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'homepage')]
+    public function homepage(): Response
+    {
+        return new Response("hnunj");
+    }
+
+    #[Route('documentation/{slug}', name: 'doc')]
+    public function show($slug): Response
+    {
+        $paragrafs = [
+            'ervrevrever',
+            'ververv',
+            'ervevervev verfef rferf'
+            ];
+
+        return $this->render('home/documentation.html.twig', [
+            'content' => sprintf('dupa blada "%s"', $slug),
+            'paragrafs' => $paragrafs
+        ]);
+    }
+}
